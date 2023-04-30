075bcd7c51ac73bd22e0bf4742c0ce4bd95c6bdd
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-04-30 20:42:03 +1000

    remove test functions

---
+++ 2_ReactJS_frontend/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Button } from "@mui/material";
+import { IoIosRocket } from "react-icons/io";
+
+function App() {
+  return (
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+      <div className="flex flex-col text-white text-center">
+        <IoIosRocket className="text-6xl mb-6" />
+        <h1 className="text-4xl mb-6">Team 57 Frontend App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
