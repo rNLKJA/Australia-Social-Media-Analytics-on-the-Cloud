a5054a9a5d7d1d4ff9fe4583432de1a0f71ccf50
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-04-30 20:41:32 +1000

    Update tailwind.config.js

--- 2_ReactJS_frontend/frontend/tailwind.config.js
+++ 2_ReactJS_frontend/frontend/tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
