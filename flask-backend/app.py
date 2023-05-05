998f58bb2639b5ae3ae741aad8cc1c70e03f5e87
Wei Zhao <55085953+EcZww@users.noreply.github.com>
2023-05-05 16:53:17 +1000

    add: dockerfile for the flask app

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!</p>'
+    return 'Hello, World!</p> <br /> <p> This is a test of the Flask backend using nginx and gunicorn.'
