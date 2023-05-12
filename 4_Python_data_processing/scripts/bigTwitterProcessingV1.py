f8a65031e379824b7bbd73c4442479ebe0a60b91
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-05-13 02:00:22 +1000

    Update bigTwitterProcessingV1.py

--- 4_Python_data_processing/scripts/bigTwitterProcessingV1.py
+++ 4_Python_data_processing/scripts/bigTwitterProcessingV1.py
@@ -14,11 +14,13 @@ from argparser import args
 from utils import generate_item, return_words_ngrams, normalise_location, load_pickle_object
 from mpi.utils import split_file_into_chunks
 from twitter.processor import twitter_processor
+from database.database import CouchDB
 
-twitter_file_path = Path(f'../data/{args.twitter_file}.json')
+twitter_file_path = Path().absolute()/'data'/f'{args.twitter_file}.json'
 
 # load processed sal.dict file
-sal_dict = load_pickle_object(Path('../data/sal.processed.dict.pkl'))
+sal_path = Path().absolute()/'data'/'sal.processed.dict.pkl'
+sal_dict = load_pickle_object(sal_path)
 
 # define MPI tools, subtask ranks and size
 comm = MPI.COMM_WORLD
@@ -26,9 +28,8 @@ rank, size = comm.Get_rank(), comm.Get_size()
 
 # return a list of specified file bytes each process need to processed
 chunk_start, chunk_end = split_file_into_chunks(twitter_file_path, size)
+db = CouchDB('twitter')
 
-twitter_processor(twitter_file_path, chunk_start[rank], chunk_end[rank], sal_dict)
-
-print(rank, chunk_start, chunk_end)
+twitter_processor(twitter_file_path, chunk_start[rank], chunk_end[rank], sal_dict, rank, db)
 
 sys.exit()
