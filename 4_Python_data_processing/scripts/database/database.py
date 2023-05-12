76f74de0209c1c4f21e31e176d9923e220fbf391
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-05-13 00:25:49 +1000

    Update database.py

--- 4_Python_data_processing/scripts/database/database.py
+++ 4_Python_data_processing/scripts/database/database.py
@@ -10,33 +10,21 @@ COUCHDB_USERNAME = os.environ['COUCHDB_USERNAME']
 COUCHDB_PASSWORD = os.environ['COUCHDB_PASSWORD']
 
 class CouchDB:
-    def __init__(self, host=COUCHDB_HOST, port=COUCHDB_PORT, 
+    def __init__(self, dbname, host=COUCHDB_HOST, port=COUCHDB_PORT,
                  username=COUCHDB_USERNAME, password=COUCHDB_PASSWORD):
-        self.host = host,
-        self.port = port,
+        self.host = host
+        self.port = port
         self.username = username
         self.password = password
 
         self.instance_url = f"http://{self.username}:{self.password}@{self.host}:{self.port}"
-        self.server = "Server Disconnected"
-    
-    def connect(self):
         self.server = couchdb.Server(self.instance_url)
-        print(self.server)
-        
+        self.db = self.get_or_create_database(dbname)
+
     def __repr__(self):
-        return f"{self.server}"
-        
-    def connect(self):
-        self.server = couchdb.Server(self.instance_url)
-        print(self.server)
-        
-    def upload_document(self, db, data, verbose=False):
-        doc_id, doc_rev = db.save(data)
-        if verbose:
-            print(f"Document uploaded with ID: {doc_id}", end='\r')
-    
-    def create_database(self, dbname):
+        return f"{self.server} - {self.db}"
+
+    def get_or_create_database(self, dbname):
         try:
             db = self.server.create(dbname)
             print(f"Database '{dbname}' created successfully.")
@@ -45,36 +33,36 @@ class CouchDB:
             print(f"Database '{dbname}' already exists.")
             return self.server[dbname]
 
-    def get_document(self, db, doc_id):
+    def upload_document(self, data, verbose=False):
+        doc_id, doc_rev = self.db.save(data)
+        if verbose:
+            print(f"Document uploaded with ID: {doc_id}", end='\r')
+        return doc_id
+
+    def get_document(self, doc_id):
         try:
-            doc = db[doc_id]
+            doc = self.db[doc_id]
             return doc
         except couchdb.http.ResourceNotFound:
             print(f"Document with ID '{doc_id}' not found.")
             return None
-    
-    def delete_document(self, db, doc_id):
+
+    def delete_document(self, doc_id):
         try:
-            doc = db[doc_id]
-            db.delete(doc)
+            doc = self.db[doc_id]
+            self.db.delete(doc)
             print(f"Document with ID '{doc_id}' deleted successfully.")
         except couchdb.http.ResourceNotFound:
             print(f"Document with ID '{doc_id}' not found.")
-    
-    def update_document(self, db, doc_id, updated_data):
-        doc = self.get_document(db, doc_id)
+
+    def update_document(self, doc_id, updated_data):
+        doc = self.get_document(doc_id)
         if doc:
             doc.update(updated_data)
-            db.save(doc)
+            self.db.save(doc)
             print(f"Document with ID '{doc_id}' updated successfully.")
         else:
             print(f"Document with ID '{doc_id}' not found.")
 
-    def list_databases(self):
-        return self.server.all_dbs()
-
-    def list_documents(self, db):
-        return [doc for doc in db.view("_all_docs")]
-
-
-
+    def list_documents(self):
+        return [doc for doc in self.db.view("_all_docs")]
