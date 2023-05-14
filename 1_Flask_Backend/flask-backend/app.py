2c31fba162722dd05e942a75fe3be61bf3c7bc39
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-05-14 19:20:52 +1000

    updated

--- 1_Flask_Backend/flask-backend/app.py
+++ 1_Flask_Backend/flask-backend/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!</p> <br /> <p> This is a test of the Flask backend using nginx and gunicorn.'
+
+@app.route('/mastodon/<string:mastodon_server>', method=['GET'])
+def mastodon_scarper(mastodon_server):
+    return f"Hello, {mastodon_server}!"
