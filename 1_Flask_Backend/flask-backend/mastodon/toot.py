ab745b3565df07e7e2f311a704db79d39da08bce
Wei Zhao <55085953+EcZww@users.noreply.github.com>
2023-05-16 02:35:10 +1000

    add: add mastodon data harverstor script

--- 1_Flask_Backend/flask-backend/mastodon/toot.py
+++ 1_Flask_Backend/flask-backend/mastodon/toot.py
@@ -1,16 +1,15 @@
-import os
-from dotenv import load_dotenv
+from utils import *
+from bs4 import BeautifulSoup
 
 class Toot:
     def __init__(self, tid=None, date=None, author=None, 
-                 lang=None, content=None, score=None, tags=None):
+                 lang=None, content=None, score=None):
         self.tid = tid
         self.date = date
         self.author = author
         self.lang = lang
         self.content = content
         self.score = score
-        self.tags = tags
         
     def __repr__(self):
         return f"Toot {self.tid}"
@@ -18,5 +17,15 @@ class Toot:
     def to_dict(self):
         data = dict(tid=self.tid, date=self.date,
                     author=self.author, lang=self.lang,
-                    content=content.self.content, score=self.score)
+                    content=self.content, score=self.score)
         return data
+
+def extract_mastodon_info(res):
+    toot_id = res.id
+    date = res.created_at.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    lang = res.language
+    content = normalize_string(BeautifulSoup(res.content, 'html.parser').text)
+    score = sentiment_analysis(content)
+    author = res['account']['id']
+    
+    return Toot(tid=toot_id, date=date, lang=lang, content=content, score=score, author=author)
