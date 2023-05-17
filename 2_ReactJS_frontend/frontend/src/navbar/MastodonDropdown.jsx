47daf4fcd65a89679d9a8f7b4eb3c0567701738b
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-05-18 00:25:44 +1000

    updated frontend data

---
+++ 2_ReactJS_frontend/frontend/src/navbar/MastodonDropdown.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { BsMastodon } from "react-icons/bs";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+const Mastodon = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button
+        variant="text"
+        startIcon={<BsMastodon />}
+        className="w-auto py-2 px-4"
+        onClick={handleClick}
+        style={{ color: "#094183" }}
+      >
+        Mastodon
+      </Button>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={() => handleOptionSelect("option1")}>
+          Option 1
+        </MenuItem>
+        <MenuItem onClick={() => handleOptionSelect("option2")}>
+          Option 2
+        </MenuItem>
+        {/* Add more menu items for different options */}
+      </Menu>
+    </div>
+  );
+};
+
+export default Mastodon;
