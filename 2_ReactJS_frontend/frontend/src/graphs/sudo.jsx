d326da515349c5efb563387c1d31b3ee36f4179c
rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-05-22 03:57:22 +1000

    Update sudo.jsx

--- 2_ReactJS_frontend/frontend/src/graphs/sudo.jsx
+++ 2_ReactJS_frontend/frontend/src/graphs/sudo.jsx
@@ -39,9 +39,9 @@ export default function SudoPlot({ sudoData }) {
           const signal = controller.signal;
 
           const response = await fetch(
-            "http://localhost:8080/api/sudo/income"
+            "http://172.26.128.118:8080/api/sudo/income"
               ? sudoData.map === "median_income_sa2"
-              : "http://localhost:8080/api/sudo/crime",
+              : "http://172.26.128.118:8080/api/sudo/crime",
             { signal },
           );
 
